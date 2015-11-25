0f6666130364bc7c8691381344f8ea8a58976eb5
Joseph Weissman <joe@bignerdranch.com>
2015-11-25 13:22:08 -0500

    rough in collaborators

--- lib/turing.rb
+++ lib/turing.rb
@@ -1,1 +1,90 @@
 require 'turing/version'
+
+module Turing
+  class TapePosition
+    def shift_right!
+      @index += 1
+      self
+    end
+
+    def shift_left!
+      @index -= 1
+      self
+    end
+
+    # protected
+
+    def index
+      @index ||= 0
+    end
+
+  end
+
+  class Tape
+    def inscribe(symbol)
+      cells[position.index] = symbol
+    end
+
+    def read_inscription
+      cells[position.index] ||= nil
+    end
+
+    def position
+      @position ||= TapePosition.new
+    end
+
+    def cells
+      @cells ||= []
+    end
+  end
+
+  class Machine
+    attr_reader :tape
+
+    def initialize
+      @tape = Tape.new
+    end
+
+    def head
+      @tape.position
+    end
+
+    def write(sym)
+      @tape.inscribe sym
+    end
+
+    def read
+      @tape.read_inscription
+    end
+
+    def run!(program)
+      iterate program until halted?
+    end
+    
+    def iterate(program)
+      raise "Invalid state '#{state}'" unless program.include?(state)
+
+      state_options = program[state]
+
+      register.push(read)
+      register.compact!
+
+      symbol, direction, @state = state_options[read] || state_options[:any]
+
+      write(symbol) if symbol
+      @tape.position.send :"shift_#{direction}!" if direction
+    end
+
+    def register
+      @registry ||= []
+    end
+
+    def state
+      @state ||= :start
+    end
+
+    def halted?
+      @state == :halt
+    end
+  end
+end
--- spec/turing_spec.rb
+++ spec/turing_spec.rb
@@ -1,8 +1,110 @@
 require 'spec_helper'
 require 'turing'
 
-describe Turing do
-  it "should have a VERSION constant" do
-    expect(subject.const_get('VERSION')).to_not be_empty
+describe TapePosition do
+  describe 'shift_left!' do
+    it 'should decrement positional index' do
+      expect { subject.shift_left! }.to change { subject.index }.by(-1)
+    end
+  end
+
+  describe 'shift_right!' do
+    it 'should increment positional index' do
+      expect { subject.shift_right! }.to change { subject.index }.by(1)
+    end
+  end
+end
+
+describe Machine do
+  describe 'structure' do
+    it 'has a .tape' do
+      expect(subject.tape).to be_a(Tape)
+    end
+
+    it 'has a .head' do
+      expect(subject.head).to be_a(TapePosition)
+    end
+  end
+
+  describe '#write' do
+    let(:symbol) { '+' }
+    it 'inscribes a symbol on the tape at head' do
+      subject.write symbol
+      expect(subject.read).to eql(symbol)
+    end
+  end
+
+  describe '#read' do
+    describe 'produces the symbol written on the tape at head' do
+      context 'when tape is empty' do
+        it 'produces nil' do
+          expect(subject.read).to be_nil
+        end
+      end
+    end
+  end
+
+  describe '#run' do
+    context 'given a simple known-to-halt program' do
+      let(:program) do
+        { 
+          :start      => { :any => [nil, nil, :next_state] },
+          :next_state => { :any => [nil, nil, :halt] }
+        }
+      end
+
+      it 'should halt' do
+        subject.run!(program)
+        aggregate_failures 'simple machine' do
+          expect(subject.state).to eql :halt
+          expect(subject.register).to all(be_empty)
+        end
+      end
+    end
+
+    context 'given a program which inscribes a symbol' do
+      let(:program) do
+        {
+          :start => { :any => [ 's', :left, :next ] },
+          :next  => { :any => [ nil, :right, :halt ] }
+        }
+      end
+
+      it 'should have the symbol on its tape' do
+        subject.run! program
+        expect(subject).to be_halted
+        expect(subject.read).to eq('s')
+      end
+    end
+
+    context 'given a program which inscribes multiple symbols' do
+      let(:program) do
+        {
+          :start => { :any => [ 'a', nil, :next ] },
+          :next  => { :any => [ 'b', nil, :stop ] },
+          :stop  => { :any => [ nil, nil, :halt ] },
+        }
+      end
+
+      before { subject.run!(program) }
+
+      it 'should have the symbol in register' do
+        expect(subject).to be_halted
+        expect(subject.read).to eq('b')
+        expect(subject.register).to eq(['a', 'b'])
+      end
+    end
+
+    context 'given a program with invalid states' do
+      let(:program) do 
+        {
+          :start => { :any => [ nil, nil, :nonexistent ] }
+        }
+      end
+
+      it 'should raise an error' do
+        expect { subject.run!(program) }.to raise_error("Invalid state 'nonexistent'")
+      end
+    end
   end
 end
