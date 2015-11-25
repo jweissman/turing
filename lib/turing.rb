10a23e7403c56798cc0561e79767e617fd46ba4a
Joseph Weissman <joe@bignerdranch.com>
2015-11-25 16:13:29 -0500

    extract state and program objects

--- lib/turing.rb
+++ lib/turing.rb
@@ -1,90 +1,184 @@
 require 'turing/version'
+require 'forwardable'
+require 'pry'
 
 module Turing
   class TapePosition
-    def shift_right!
-      @index += 1
-      self
+    def shift(direction)
+      if direction == :right
+        @index += 1
+      elsif direction == :left
+        @index -= 1
+      end
+    end
+    
+    def find(list)
+      list[index]
     end
 
-    def shift_left!
-      @index -= 1
-      self
-    end
-
-    # protected
+    protected
 
     def index
       @index ||= 0
     end
+  end
 
+  class Cell
+    def read
+      inscribed?
+    end
+
+    def write
+      @inscribed = true
+    end
+
+    def erase
+      @inscribed = false
+    end
+
+    def inscribed?
+      @inscribed ||= false
+    end
   end
 
   class Tape
-    def inscribe(symbol)
-      cells[position.index] = symbol
+    extend Forwardable
+    def_delegators :current_cell, :read, :write, :erase
+    def_delegator :position, :shift, :move
+
+    def current_cell
+      position.find(cells)
     end
 
-    def read_inscription
-      cells[position.index] ||= nil
-    end
+    protected
 
     def position
       @position ||= TapePosition.new
     end
 
     def cells
-      @cells ||= []
+      @cells ||= [Cell.new]
+    end
+  end
+
+  class Instruction
+    attr_reader :symbol
+    attr_reader :next_state, :should_write, :direction
+
+    def initialize(symbol, should_write: nil, direction: nil, next_state: :halt)
+      @symbol = symbol
+      @should_write = should_write
+      @direction = direction
+      @next_state = next_state
+    end
+
+    def write?
+      !!should_write
+    end
+  end
+
+  class State
+    attr_reader :name, :instructions
+
+    def initialize(name)
+      @name = name
+      @instructions = []
+      yield self if block_given?
+    end
+
+    def on(sym, *opts)
+      @instructions << Instruction.new(sym, *opts)
+      self
+    end
+
+    def instruction_table
+      table = {}
+      @instructions.each do |instruction|
+        table[instruction.symbol] = instruction
+      end
+      table
+    end
+  end
+
+  class ProgramValidator
+    def check(program)
+      state_names = program.state_names
+      program.states.each do |state|
+        state.instructions.each do |instruction|
+          unless state_names.include?(instruction.next_state)
+            raise "Invalid state '#{instruction.next_state}'" 
+          end
+        end
+      end
+    end
+  end
+
+  class Program
+    attr_reader :states
+
+    def initialize
+      yield self if block_given?
+      validator.check self
+    end
+
+    def state(name,*opts,&blk)
+      @states ||= [halt_state]
+      new_state = State.new(name,*opts,&blk)
+      @states.push new_state
+    end
+
+    def find(name)
+      @states.detect { |state| state.name == name }
+    end
+
+    def state_names
+      @states.collect(&:name)
+    end
+
+    def halt_state
+      State.new :halt
+    end
+
+    protected
+
+    def validator
+      @validator ||= ProgramValidator.new
     end
   end
 
   class Machine
-    attr_reader :tape
+    extend Forwardable
+    def_delegators :tape, :read, :write, :erase, :move
 
-    def initialize
-      @tape = Tape.new
-    end
-
-    def head
-      @tape.position
-    end
-
-    def write(sym)
-      @tape.inscribe sym
-    end
-
-    def read
-      @tape.read_inscription
+    def tape
+      @tape ||= Tape.new
     end
 
     def run!(program)
-      iterate program until halted?
+      @state = program.find :start
+      @state = iterate program until halted?
+    end
+
+    def validate(program)
+      program_validator.check(program)
     end
     
     def iterate(program)
-      raise "Invalid state '#{state}'" unless program.include?(state)
-
-      state_options = program[state]
-
-      register.push(read)
-      register.compact!
-
-      symbol, direction, @state = state_options[read] || state_options[:any]
-
-      write(symbol) if symbol
-      @tape.position.send :"shift_#{direction}!" if direction
-    end
-
-    def register
-      @registry ||= []
-    end
-
-    def state
-      @state ||= :start
+      instruction = next_instruction
+      write if instruction.write?
+      move instruction.direction if instruction.direction
+      program.find instruction.next_state
     end
 
     def halted?
-      @state == :halt
+      @state.name == :halt
+    end
+
+    protected
+
+    def next_instruction
+      state_options = @state.instruction_table
+      state_options[read] || state_options[:any]
     end
   end
 end
--- spec/turing_spec.rb
+++ spec/turing_spec.rb
@@ -2,17 +2,58 @@ require 'spec_helper'
 require 'turing'
 
 describe TapePosition do
-  describe 'shift_left!' do
-    it 'should decrement positional index' do
-      expect { subject.shift_left! }.to change { subject.index }.by(-1)
+  describe 'shift' do
+    context ':left' do
+      it 'should decrement positional index' do
+        expect { subject.shift(:left) }.to change { subject.send :index }.by(-1)
+      end
+    end
+
+    context ':right' do
+      it 'should increment positional index' do
+        expect { subject.shift(:right) }.to change { subject.send :index }.by(1)
+      end
+    end
+  end
+end
+
+describe Cell do
+  describe '#inscribed?' do
+    it 'starts off false' do
+      expect(subject).not_to be_inscribed
+    end
+  end
+end
+
+describe Tape do
+  describe 'structure' do
+    let(:position) { subject.send :position }
+    let(:cells)    { subject.send :cells }
+
+    it 'has a position' do
+      expect(position).to be_a(TapePosition)
+    end
+
+    it 'has cells' do
+      expect(cells).not_to be_empty
+      expect(cells).to all(be_a(Cell))
     end
   end
 
-  describe 'shift_right!' do
-    it 'should increment positional index' do
-      expect { subject.shift_right! }.to change { subject.index }.by(1)
-    end
-  end
+  # describe '#scan' do
+  #   it 'reads a sequence from the tape' do
+  #     subject.position.shift(:right)
+  #     subject.position.shift(:right)
+  #     subject.position
+
+  #     
+  #   end
+  # end
+
+  # describe '#prepare' do
+  #   it 'inscribes a sequence onto the tape and rewinds it' do
+  #   end
+  # end
 end
 
 describe Machine do
@@ -20,90 +61,93 @@ describe Machine do
     it 'has a .tape' do
       expect(subject.tape).to be_a(Tape)
     end
-
-    it 'has a .head' do
-      expect(subject.head).to be_a(TapePosition)
-    end
   end
 
   describe '#write' do
-    let(:symbol) { '+' }
-    it 'inscribes a symbol on the tape at head' do
-      subject.write symbol
-      expect(subject.read).to eql(symbol)
+    it 'writes the tape at head' do
+      subject.write
+      expect(subject.read).to be true
+    end
+  end
+
+  describe '#erase' do
+    before { subject.write }
+    it 'erases an inscription at head' do
+      subject.erase
+      expect(subject.read).to be false
     end
   end
 
   describe '#read' do
-    describe 'produces the symbol written on the tape at head' do
+    describe 'indicates whether the tape is writed at head' do
       context 'when tape is empty' do
-        it 'produces nil' do
-          expect(subject.read).to be_nil
+        it 'produces false' do
+          expect(subject.read).to be false
         end
       end
     end
   end
+  
 
   describe '#run' do
-    context 'given a simple known-to-halt program' do
-      let(:program) do
-        { 
-          :start      => { :any => [nil, nil, :next_state] },
-          :next_state => { :any => [nil, nil, :halt] }
-        }
-      end
-
-      it 'should halt' do
-        subject.run!(program)
-        aggregate_failures 'simple machine' do
-          expect(subject.state).to eql :halt
-          expect(subject.register).to all(be_empty)
-        end
-      end
-    end
-
-    context 'given a program which inscribes a symbol' do
-      let(:program) do
-        {
-          :start => { :any => [ 's', :left, :next ] },
-          :next  => { :any => [ nil, :right, :halt ] }
-        }
-      end
-
-      it 'should have the symbol on its tape' do
-        subject.run! program
-        expect(subject).to be_halted
-        expect(subject.read).to eq('s')
-      end
-    end
-
-    context 'given a program which inscribes multiple symbols' do
-      let(:program) do
-        {
-          :start => { :any => [ 'a', nil, :next ] },
-          :next  => { :any => [ 'b', nil, :stop ] },
-          :stop  => { :any => [ nil, nil, :halt ] },
-        }
-      end
-
-      before { subject.run!(program) }
-
-      it 'should have the symbol in register' do
-        expect(subject).to be_halted
-        expect(subject.read).to eq('b')
-        expect(subject.register).to eq(['a', 'b'])
-      end
-    end
-
     context 'given a program with invalid states' do
+      let(:inexistent_state) { 'some nonexistent state' }
+      let(:expected_message) { "Invalid state '#{inexistent_state}'" }
+
       let(:program) do 
-        {
-          :start => { :any => [ nil, nil, :nonexistent ] }
-        }
+        Program.new do |program|
+          program.state(:start) do |start|
+            start.on(:any, next_state: inexistent_state)
+          end
+        end
       end
 
       it 'should raise an error' do
-        expect { subject.run!(program) }.to raise_error("Invalid state 'nonexistent'")
+        expect { subject.run!(program) }.to raise_error(expected_message)
+      end
+    end
+
+    context 'given coherent programs known to halt by inspection' do
+      before { subject.run!(program) }
+
+      context 'given a simple program' do
+        let(:program) do
+          Program.new do |program|
+            program.state(:start) do |s|
+              s.on(:any, next_state: :one)
+            end
+
+            program.state(:one) do |state|
+              state.on :any, next_state: :halt
+            end
+          end
+        end
+
+        it 'should halt' do
+          expect(subject).to be_halted
+        end
+      end
+
+      context 'given a program which writes a symbol' do
+        let(:program) do
+          Program.new do |program|
+            program.state :start do |start|
+              start.on :any, should_write: true, direction: :left, next_state: :one
+            end
+
+            program.state :one do |one|
+              one.on :any, direction: :right, next_state: :halt
+            end
+          end
+        end
+
+        it 'should halt' do
+          expect(subject).to be_halted
+        end
+
+        it 'should have an inscription on its tape' do
+          expect(subject.read).to be true
+        end
       end
     end
   end
