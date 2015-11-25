2424af4e72b59863650a12045b5ded95823093c9
Joseph Weissman <joe@bignerdranch.com>
2015-11-25 17:39:12 -0500

    organize into modules, build out tests

--- lib/turing.rb
+++ lib/turing.rb
@@ -3,182 +3,207 @@ require 'forwardable'
 require 'pry'
 
 module Turing
-  class TapePosition
-    def shift(direction)
-      if direction == :right
-        @index += 1
-      elsif direction == :left
-        @index -= 1
+  module Memory
+    class Cell
+      def read
+        inscribed?
+      end
+
+      def write
+        @inscribed = true
+      end
+
+      def erase
+        @inscribed = false
+      end
+
+      def inscribed?
+        @inscribed ||= false
       end
     end
-    
-    def find(list)
-      list[index]
-    end
 
-    protected
-
-    def index
-      @index ||= 0
-    end
-  end
-
-  class Cell
-    def read
-      inscribed?
-    end
-
-    def write
-      @inscribed = true
-    end
-
-    def erase
-      @inscribed = false
-    end
-
-    def inscribed?
-      @inscribed ||= false
-    end
-  end
-
-  class Tape
-    extend Forwardable
-    def_delegators :current_cell, :read, :write, :erase
-    def_delegator :position, :shift, :move
-
-    def current_cell
-      position.find(cells)
-    end
-
-    protected
-
-    def position
-      @position ||= TapePosition.new
-    end
-
-    def cells
-      @cells ||= [Cell.new]
-    end
-  end
-
-  class Instruction
-    attr_reader :symbol
-    attr_reader :next_state, :should_write, :direction
-
-    def initialize(symbol, should_write: nil, direction: nil, next_state: :halt)
-      @symbol = symbol
-      @should_write = should_write
-      @direction = direction
-      @next_state = next_state
-    end
-
-    def write?
-      !!should_write
-    end
-  end
-
-  class State
-    attr_reader :name, :instructions
-
-    def initialize(name)
-      @name = name
-      @instructions = []
-      yield self if block_given?
-    end
-
-    def on(sym, *opts)
-      @instructions << Instruction.new(sym, *opts)
-      self
-    end
-
-    def instruction_table
-      table = {}
-      @instructions.each do |instruction|
-        table[instruction.symbol] = instruction
+    class TapePosition
+      def shift(direction)
+        if direction == :right
+          @index = index + 1
+        elsif direction == :left
+          @index = index - 1
+        end
       end
-      table
+      
+      def find(hash)
+        hash[index] ||= Cell.new
+      end
+
+      protected
+
+      def index
+        @index ||= 0
+      end
+    end
+
+    class Tape
+      extend Forwardable
+      def_delegators :current_cell, :read, :write, :erase
+      def_delegator :position, :shift, :move
+
+      def current_cell
+        position.find(cells)
+      end
+
+      def position
+        @position ||= TapePosition.new
+      end
+
+      def cells
+        @cells ||= {}
+      end
     end
   end
 
-  class ProgramValidator
-    def check(program)
-      state_names = program.state_names
-      program.states.each do |state|
-        state.instructions.each do |instruction|
-          unless state_names.include?(instruction.next_state)
-            raise "Invalid state '#{instruction.next_state}'" 
-          end
+  module Processor
+    class Instruction
+      attr_reader :symbol
+      attr_reader :next_state, :should_write, :direction
+
+      def initialize(symbol, should_write: nil, direction: nil, next_state: :halt)
+        @symbol = symbol
+        @should_write = should_write
+        @direction = direction
+        @next_state = next_state
+      end
+
+      def write?
+        !!should_write
+      end
+    end
+
+    class State
+      attr_reader :name, :instructions
+
+      def initialize(name)
+        @name = name
+        @instructions = []
+        yield self if block_given?
+      end
+
+      def on(sym, *opts)
+        @instructions << Instruction.new(sym, *opts)
+        self
+      end
+
+      def instruction_table
+        @instructions.inject({}) do |hash, instruction|
+          hash[instruction.symbol] = instruction
+          hash
         end
       end
     end
-  end
 
-  class Program
-    attr_reader :states
+    class ProgramValidator
+      def check(program)
+        instructions = program.states.collect(&:instructions).flatten
+        instructions.each do |instruction|
+          check_instruction instruction, program.state_names
+        end
+      end
 
-    def initialize
-      yield self if block_given?
-      validator.check self
+      def check_instruction(instruction, state_names)
+        state = instruction.next_state
+        raise "Invalid state '#{state}'" unless state_names.include? state
+      end
     end
 
-    def state(name,*opts,&blk)
-      @states ||= [halt_state]
-      new_state = State.new(name,*opts,&blk)
-      @states.push new_state
+    class Program
+      attr_reader :states
+
+      def initialize
+        yield self if block_given?
+        validator.check self
+      end
+
+      def state(name,*opts,&blk)
+        @states ||= [halt_state]
+        new_state = State.new(name,*opts,&blk)
+        @states.push new_state
+      end
+
+      def find(name)
+        @states.detect { |state| state.name == name }
+      end
+
+      def state_names
+        @states.collect(&:name)
+      end
+
+      def halt_state
+        State.new :halt
+      end
+
+      protected
+
+      def validator
+        @validator ||= ProgramValidator.new
+      end
     end
 
-    def find(name)
-      @states.detect { |state| state.name == name }
-    end
+    class Machine
+      EXECUTION_LIMIT = 10_000
 
-    def state_names
-      @states.collect(&:name)
-    end
+      attr_reader :counter
 
-    def halt_state
-      State.new :halt
-    end
+      extend Forwardable
+      def_delegators :tape, :read, :write, :erase, :move
 
-    protected
+      def run!(program)
+        validate program
 
-    def validator
-      @validator ||= ProgramValidator.new
-    end
-  end
+        @state = program.find :start
+        @state = iterate program until stop_iteration?
+      end
 
-  class Machine
-    extend Forwardable
-    def_delegators :tape, :read, :write, :erase, :move
+      def halted?
+        @state.name == :halt
+      end
 
-    def tape
-      @tape ||= Tape.new
-    end
+      protected
+ 
+      def tape
+        @tape ||= Tape.new
+      end     
 
-    def run!(program)
-      @state = program.find :start
-      @state = iterate program until halted?
-    end
+      def stop_iteration?
+        halted? || (@counter ||= 0) >= EXECUTION_LIMIT
+      end
 
-    def validate(program)
-      program_validator.check(program)
-    end
-    
-    def iterate(program)
-      instruction = next_instruction
-      write if instruction.write?
-      move instruction.direction if instruction.direction
-      program.find instruction.next_state
-    end
+      def validate(program)
+        program_validator.check(program)
+      end
 
-    def halted?
-      @state.name == :halt
-    end
+      def tick!
+        @counter ||= 0
+        @counter += 1
+      end
+      
+      def iterate(program)
+        tick!
 
-    protected
+        instruction = next_instruction
+        write if instruction.write?
+        move instruction.direction if instruction.direction
+        program.find instruction.next_state
+      end
 
-    def next_instruction
-      state_options = @state.instruction_table
-      state_options[read] || state_options[:any]
+      protected
+
+      def program_validator
+        @validator ||= ProgramValidator.new
+      end
+
+      def next_instruction
+        state_options = @state.instruction_table
+        state_options[read] || state_options[:any]
+      end
     end
   end
 end
--- spec/turing_spec.rb
+++ spec/turing_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 require 'turing'
 
+include Turing::Memory
+include Turing::Processor
+
 describe TapePosition do
   describe 'shift' do
     context ':left' do
@@ -23,30 +26,74 @@ describe Cell do
       expect(subject).not_to be_inscribed
     end
   end
+
+  describe '#write' do
+    before { subject.write }
+    it 'marks the cell' do
+      expect(subject).to be_inscribed
+    end
+  end
+
+  describe '#erase' do
+    context 'when nothing has been written' do
+      before { subject.erase }
+      it 'unmarks the cell' do
+        expect(subject).not_to be_inscribed
+      end
+    end
+
+    context 'when something has been written' do
+      before do 
+        subject.write 
+        subject.erase 
+      end
+
+      it 'unmarks the cell' do
+        expect(subject).not_to be_inscribed
+      end
+    end
+  end
 end
 
 describe Tape do
   describe 'structure' do
-    let(:position) { subject.send :position }
-    let(:cells)    { subject.send :cells }
 
     it 'has a position' do
-      expect(position).to be_a(TapePosition)
+      expect(subject.position).to be_a(TapePosition)
     end
 
-    it 'has cells' do
-      expect(cells).not_to be_empty
-      expect(cells).to all(be_a(Cell))
+    # it 'has cells' do
+    #   expect(subject.cells).not_to be_empty
+    #   expect(subject.cells).to all(be_a(Cell))
+    # end
+  end
+
+  describe '#current_cell' do
+    it 'calls #find on position with cells' do
+      position = instance_spy('TapePosition')
+      expect(subject).to receive(:position).and_return(position)
+      
+      subject.current_cell
+
+      expect(position).to have_received(:find).with(subject.cells)
     end
   end
 
   # describe '#scan' do
-  #   it 'reads a sequence from the tape' do
+  #   before do
   #     subject.position.shift(:right)
   #     subject.position.shift(:right)
-  #     subject.position
+  #     subject.position.shift(:right)
+  #     subject.write
+  #     subject.position.shift(:left)
+  #     subject.erase
+  #     subject.position.shift(:left)
+  #     subject.write
+  #     subject.position.shift(:left)
+  #   end
 
-  #     
+  #   it 'reads a sequence left-to-right from the tape' do
+  #     expect(subject.scan).to eql([1,0,1])
   #   end
   # end
 
@@ -57,12 +104,6 @@ describe Tape do
 end
 
 describe Machine do
-  describe 'structure' do
-    it 'has a .tape' do
-      expect(subject.tape).to be_a(Tape)
-    end
-  end
-
   describe '#write' do
     it 'writes the tape at head' do
       subject.write
@@ -79,7 +120,7 @@ describe Machine do
   end
 
   describe '#read' do
-    describe 'indicates whether the tape is writed at head' do
+    describe 'indicates whether the tape is inscribed at head' do
       context 'when tape is empty' do
         it 'produces false' do
           expect(subject.read).to be false
@@ -88,22 +129,47 @@ describe Machine do
     end
   end
   
+  describe '#validate' do
+    let(:inexistent_state) { 'some nonexistent state' }
+    let(:expected_message) { "Invalid state '#{inexistent_state}'" }
 
-  describe '#run' do
-    context 'given a program with invalid states' do
-      let(:inexistent_state) { 'some nonexistent state' }
-      let(:expected_message) { "Invalid state '#{inexistent_state}'" }
-
-      let(:program) do 
-        Program.new do |program|
-          program.state(:start) do |start|
-            start.on(:any, next_state: inexistent_state)
-          end
+    let(:program) do 
+      Program.new do |program|
+        program.state(:start) do |start|
+          start.on(:any, next_state: inexistent_state)
         end
       end
+    end
 
-      it 'should raise an error' do
-        expect { subject.run!(program) }.to raise_error(expected_message)
+    it 'should identify programs with invalid states' do
+      expect { subject.validate(program) }.to raise_error(expected_message)
+    end
+  end
+
+  describe '#run' do
+    context 'collaboration' do
+      let(:instruction) do
+        instance_double('Instruction', write?: false, direction: nil, next_state: :halt)
+      end
+
+      let(:start) do
+        instance_double('State', name: :start, instruction_table: {
+          any: instruction
+        })
+      end
+
+      let(:program) do
+        instance_double('Program', find: start)
+      end
+
+      let(:validator) do
+        instance_spy('ProgramValidator')
+      end
+
+      it 'should check the program with the validator' do
+        expect(subject).to receive(:program_validator).and_return(validator)
+        subject.run!(program)
+        expect(validator).to have_received(:check).with(program)
       end
     end
 
@@ -148,6 +214,29 @@ describe Machine do
         it 'should have an inscription on its tape' do
           expect(subject.read).to be true
         end
+      end
+    end
+
+    context 'given a coherent program which loops forever' do
+      before { subject.run!(program) }
+      let(:program) do
+        Program.new do |program|
+          program.state :start do |start|
+            start.on :any, next_state: :restart
+          end
+
+          program.state :restart do |state|
+            state.on :any, next_state: :start
+          end
+        end
+      end
+
+      it 'should not halt' do
+        expect(subject).not_to be_halted
+      end
+
+      it 'should have maxed out the step counter' do
+        expect(subject.counter).to eql(Machine::EXECUTION_LIMIT)
       end
     end
   end
